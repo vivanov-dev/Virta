a671640d710a139641be5c49ab84170a08c21e04
vlivanov <vlad.i999913@gmail.com>
2023-01-26 10:48:08 +0200

    feature: Application version without tests

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+import Logo from "../assets/virtaLogo.svg";
+
+type Props = {
+    
+};
+
+const Header = (props: Props) => {
+    return (
+        <div>
+            <Logo/>
+        </div>
+    );
+};
+
+export { Header };
